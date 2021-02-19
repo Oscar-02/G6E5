189f97254e8cead19b2080322e24f859dff4e0af
Oscar-02 <oscacanas29@gmail.com>
2021-02-18 20:32:12 -0600

    1.0 Final Version

---
+++ G6E5/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace G6E5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i = 0;
+            for (i = 1; i < 25; i++)
+            {
+                Console.WriteLine(i);
+            }
+            for (i = 26; i < 31; i++)
+            {
+                Console.WriteLine(i);
+            }
+            Console.ReadKey();
+        }
+    }
+}
